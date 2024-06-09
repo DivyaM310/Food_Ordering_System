d2618439b4e4b657bfa1e5998735569ec6632900
DivyaM310 <mulikdivya1@gmail.com>
2024-06-09 17:01:10 +0530

    second

---
+++ Foody/src/foody/ConfirmedController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foody;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+
+/**
+ * FXML Controller class
+ *
+ * @author Winston
+ */
+public class ConfirmedController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+    @FXML
+     private void exitScreen(ActionEvent event){
+        System.exit(0);
+    }
+    
+}
